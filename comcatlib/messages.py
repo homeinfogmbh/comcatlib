b9317d508b0525a9a386e1c1e403d1e4aa6b96ab
Richard Neumann <r.neumann@homeinfo.de>
2019-01-25 12:06:57 +0100

    Refactored messages for new message API.

--- comcatlib/messages.py
+++ comcatlib/messages.py
@@ -15,3 +15,6 @@ NO_SUCH_CUSTOMER = COMCAT_MESSAGE(
     'The requested customer does not exists.', status=404)
 SESSION_EXPIRED = COMCAT_MESSAGE('Session expired.', status=401)
 ACCOUNT_LOCKED = COMCAT_MESSAGE('This account is locked.', status=401)
+ACCOUNT_ADDED = COMCAT_MESSAGE('The account has been added.', status=201)
+ACCOUNT_DELETED = COMCAT_MESSAGE('The account has been deleted.', status=200)
+ACCOUNT_PATCHED = COMCAT_MESSAGE('The account has been modified.', status=200)
