cfa9f564a22bb64614e54230ab60e82d90b897b2
Richard Neumann <r.neumann@homeinfo.de>
2019-01-29 12:17:34 +0100

    Added context locals to __init__.py.

--- comcatlib/__init__.py
+++ comcatlib/__init__.py
@@ -1,8 +1,15 @@
 """Comcat backend."""
 
 from comcatlib.auth import authenticated
+from comcatlib.contextlocals import ACCOUNT, CUSTOMER, SESSION
 from comcatlib.functions import get_session_duration
 from comcatlib.orm import Account
 
 
-__all__ = ['authenticated', 'get_session_duration', 'Account']
+__all__ = [
+    'ACCOUNT',
+    'CUSTOMER',
+    'SESSION',
+    'authenticated',
+    'get_session_duration',
+    'Account']
