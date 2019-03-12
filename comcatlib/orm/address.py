1d09ee7866753b770e1d1fae53dae41ddc9eab8e
Richard Neumann <r.neumann@homeinfo.de>
2019-03-12 16:32:57 +0100

    Implemented Address.from_json().

--- comcatlib/orm/address.py
+++ comcatlib/orm/address.py
@@ -1,5 +1,7 @@
 """ComCat addresses."""
 
+from contextlib import suppress
+
 from peewee import CharField, ForeignKeyField
 
 from mdb import Customer
@@ -18,3 +20,25 @@ class Address(ComCatModel):
     house_number = CharField(8)
     zip_code = CharField(32)
     city = CharField(64)
+
+    @classmethod
+    def from_json(cls, json, customer, unique=True, **kwargs):
+        """Adds a new address from JSON."""
+        address = super().from_json(json, **kwargs)
+
+        if unique:
+            with suppress(cls.DoesNotExist):
+                return cls.get(
+                    (cls.customer == customer)
+                    & (cls.street == address.street)
+                    & (cls.house_number == address.house_number)
+                    & (cls.zip_code == address.zip_code)
+                    & (cls.city == address.city))
+
+        address.customer = customer
+        return address
+
+    @classmethod
+    def patch_json(cls, *_, **__):
+        """Prohibit patching."""
+        raise NotImplementedError('Patching not implemented.')
