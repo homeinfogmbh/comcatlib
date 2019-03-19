9097c10de8b74f02c6cf57f281f354999f4854a9
Richard Neumann <r.neumann@homeinfo.de>
2019-03-19 13:16:22 +0100

    Refac.

--- comcatlib/facebook.py
+++ comcatlib/facebook.py
@@ -9,20 +9,26 @@ from comcatlib.presentation import Presentation
 from comcatlib.urlproxy import encode_url
 
 
-__all__ = ['get_posts']
+__all__ = ['get_accounts', 'get_posts']
 
 
-def get_posts(account):
-    """Yields posts for the respective account."""
+def get_accounts(account):
+    """Yields facebook accounts."""
 
     presentation = Presentation(Account[account.id])
 
     for chart in filter(partial(isinstance, Facebook), presentation.charts):
-        for account in chart.accounts:  # pylint: disable=R1704
-            for post in account.posts:
-                if post.image:
-                    post.image = encode_url(post.image)
-                else:
-                    post.image = None
+        for facebook_account in chart.accounts:
+            yield facebook_account
 
-                yield post
+
+def get_posts(facebook_account):
+    """Yields posts for the respective facebook account."""
+
+    for post in facebook_account.posts:
+        if post.image:
+            post.image = encode_url(post.image)
+        else:
+            post.image = None
+
+        yield post
