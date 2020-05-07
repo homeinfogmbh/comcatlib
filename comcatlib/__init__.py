302d92f8ffa903b0c0b4d8df599f86debf0a0ab5
Richard Neumann <mail@richard-neumann.de>
2020-05-07 13:08:52 +0200

    Added File to comcatlib exports.

--- comcatlib/__init__.py
+++ comcatlib/__init__.py
@@ -5,13 +5,14 @@ from comcatlib.damage_report import list_ as list_damage_reports
 from comcatlib.damage_report import submit as submit_damage_reports
 from comcatlib.oauth import REQUIRE_OAUTH, SERVER, init_oauth
 from comcatlib.orm import Address
+from comcatlib.orm import File
+from comcatlib.orm import GroupMemberUser
+from comcatlib.orm import Tenement
+from comcatlib.orm import User
 from comcatlib.orm import UserBaseChart
 from comcatlib.orm import UserConfiguration
 from comcatlib.orm import UserDamageReport
 from comcatlib.orm import UserMenu
-from comcatlib.orm import GroupMemberUser
-from comcatlib.orm import Tenement
-from comcatlib.orm import User
 from comcatlib.presentation import Presentation
 from comcatlib.urlproxy import decode_url, encode_url, proxy_url
 
@@ -26,13 +27,14 @@ __all__ = [
     'decode_url',
     'encode_url',
     'proxy_url',
+    'Address',
+    'File',
+    'GroupMemberUser',
+    'Tenement',
     'User',
     'UserBaseChart',
     'UserConfiguration',
-    'UserMenu',
     'UserDamageReport',
-    'Address',
-    'GroupMemberUser',
-    'Tenement',
+    'UserMenu',
     'Presentation'
 ]
