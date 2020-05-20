cfa2f55236b06f5941cfac5ad4e2c1bd4802f077
Richard Neumann <mail@richard-neumann.de>
2020-05-20 13:52:03 +0200

    Refac.

--- comcatlib/orm/damage_report.py
+++ comcatlib/orm/damage_report.py
@@ -5,6 +5,7 @@ from peewee import BooleanField, ForeignKeyField
 from damage_report import DamageReport
 
 from comcatlib.orm.common import ComCatModel
+from comcatlib.orm.files import File
 from comcatlib.orm.user import User
 
 
@@ -17,7 +18,16 @@ class UserDamageReport(ComCatModel):
     class Meta:     # pylint: disable=C0111,R0903
         table_name = 'user_damage_report'
 
-    user = ForeignKeyField(User, column_name='user', on_delete='CASCADE')
     damage_report = ForeignKeyField(
         DamageReport, column_name='damage_report', on_delete='CASCADE')
+    user = ForeignKeyField(User, column_name='user', on_delete='CASCADE')
     private = BooleanField(default=True)
+
+
+class DamageReportAttachment(ComCatModel):
+    """Maps an attachment onto a user damage report."""
+
+    user_damage_report = ForeignKeyField(
+        UserDamageReport, column_name='user_damage_report',
+        on_delete='CASCADE')
+    file = ForeignKeyField(File, column_name='file', on_delete='CASCADE')
