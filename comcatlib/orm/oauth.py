cac3c03ff3d6267c133473912e8d4eb66ed990bb
Richard Neumann <r.neumann@homeinfo.de>
2020-07-21 17:19:47 +0200

    Added Client.from_json() for client registration.

--- comcatlib/orm/oauth.py
+++ comcatlib/orm/oauth.py
@@ -1,8 +1,13 @@
 """ORM models based on MySQL Alchemy."""
 
+from random import choices
+from string import ascii_letters, digits
+from uuid import uuid4
+
 from peewee import ForeignKeyField
 
 from authlib.common.security import generate_token
+from peeweeplus import Transaction
 from peeweeplus.authlib import OAuth2ClientMixin
 from peeweeplus.authlib import OAuth2TokenMixin
 from peeweeplus.authlib import OAuth2AuthorizationCodeMixin
@@ -28,6 +33,47 @@ class Client(ComCatModel, OAuth2ClientMixin):   # pylint: disable=R0901
     """An OAuth client."""
 
     user = ForeignKeyField(User, column_name='user', on_delete='CASCADE')
+
+    @classmethod
+    def from_json(cls, json, **kwargs):
+        """Creates a new client from a JSON-ish dict."""
+        if json.get('clientId', None) is not None:
+            raise ValueError('Setting of client ID is not allowed.')
+
+        if json.get('clientSecret', None) is not None:
+            raise ValueError('Setting of client secret is not allowed.')
+
+        redirect_uris = json.pop('redirectURIs', None) or ()
+        grant_types = json.pop('grantTypes', None) or ()
+        response_types = json.pop('responseTypes', None) or ()
+        scopes = json.pop('scopes', None) or ()
+        contacts = json.pop('contacts', None) or ()
+        jwks = json.pop('jwks', None) or ()
+        client = cls.from_json(json, **kwargs)
+        client.client_id = uuid4().hex
+        client.client_secret = choices(ascii_letters+digits, k=32)
+        models = Transaction()
+        models.add(client, primary=True)
+
+        for uri in redirect_uris:
+            models.add(RedirectURI(client=client, uri=uri))
+
+        for typ in grant_types:
+            models.add(GrantType(client=client, type=typ))
+
+        for typ in response_types:
+            models.add(ResponseType(client=client, type=typ))
+
+        for scope in scopes:
+            models.add(Scope(client=client, scope=scope))
+
+        for contact in contacts:
+            models.add(Contact(client=client, contact=contact))
+
+        for jwk in jwks:
+            models.add(JWKS(client=client, jwk=jwk))
+
+        return models
 
 
 RedirectURI, GrantType, ResponseType, Scope, Contact, JWKS = \
