cfd2f94bc3f6b24a8b41e8437d2bfefff3374bc4
Richard Neumann <r.neumann@homeinfo.de>
2020-08-05 09:57:53 +0200

    Fixed table name.

--- comcatlib/orm/tenant2tenant.py
+++ comcatlib/orm/tenant2tenant.py
@@ -11,8 +11,11 @@ from comcatlib.orm.user import User
 __all__ = ['UserTenantMessage']
 
 
-class UserTenantMessage(ComCatModel):
+class UserTenantMessage(ComCatModel):   # pylint: disable=R0903
     """A user-issued tenant-to-tenant message."""
+
+    class Meta:     # pylint: disable=C0111,R0903
+        table_name = 'user_tenant_message'
 
     tenant_message = ForeignKeyField(
         TenantMessage, column_name='tenant_message', on_delete='CASCADE')
