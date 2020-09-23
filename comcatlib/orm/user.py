7a75e1e592eeaf0ce4066745b60e03eda73415a0
Richard Neumann <r.neumann@homeinfo.de>
2020-09-23 15:13:44 +0200

    Refactored user.

--- comcatlib/orm/user.py
+++ comcatlib/orm/user.py
@@ -1,20 +1,14 @@
 """ComCat accounts."""
 
 from datetime import datetime
-from uuid import uuid4
 
-from argon2.exceptions import VerifyMismatchError
 from peewee import BooleanField
 from peewee import DateTimeField
 from peewee import ForeignKeyField
-from peewee import IntegerField
-from peewee import UUIDField
 
 from his import CUSTOMER
-from mdb import Customer, Tenement
-from peeweeplus import Argon2Field
+from mdb import Tenement
 
-from comcatlib.exceptions import InvalidCredentials, UserLocked
 from comcatlib.functions import get_tenement
 from comcatlib.messages import NO_SUCH_USER
 from comcatlib.orm.common import ComCatModel
@@ -23,14 +17,14 @@ from comcatlib.orm.common import ComCatModel
 __all__ = ['get_user', 'User']
 
 
-MAX_FAILED_LOGINS = 5
-
-
 def get_user(ident):
     """Returns the respective user."""
 
+    condition = User.id == ident
+    condition &= Tenement.customer == CUSTOMER.id
+
     try:
-        return User.get((User.id == ident) & (User.customer == CUSTOMER.id))
+        return User.select().join(Tenement).where(condition).get()
     except User.DoesNotExist:
         raise NO_SUCH_USER
 
@@ -38,27 +32,11 @@ def get_user(ident):
 class User(ComCatModel):
     """A ComCat user."""
 
-    uuid = UUIDField(default=uuid4)
-    passwd = Argon2Field(null=True)
-    customer = ForeignKeyField(Customer, column_name='customer')
     tenement = ForeignKeyField(Tenement, column_name='tenement')
     created = DateTimeField(default=datetime.now)
-    last_login = DateTimeField(null=True)
-    failed_logins = IntegerField(default=0)
     expires = DateTimeField(null=True)
     locked = BooleanField(default=False)
     admin = BooleanField(default=False)     # Admin across entire customer.
-    root = BooleanField(default=False)      # Admin across all users.
-
-    @classmethod
-    def add(cls, customer, tenement=None, passwd=None):
-        """Creates a new user."""
-        user = cls()
-        user.customer = customer
-        user.tenement = tenement
-        user.passwd = passwd
-        user.save()
-        return user
 
     @classmethod
     def from_json(cls, json, customer, **kwargs):
@@ -77,58 +55,22 @@ class User(ComCatModel):
     @property
     def valid(self):
         """Determines whether the user may be used."""
-        return not self.locked and not self.expired and self.passwd is not None
-
-    @property
-    def can_login(self):
-        """Determines whether the user may login."""
-        return self.valid and self.failed_logins <= MAX_FAILED_LOGINS
-
-    @property
-    def instance(self):
-        """Returns the user instance.
-        This is used to get the actual
-        user model from a LocalProxy.
-        """
-        return self
-
-    def login(self, passwd):
-        """Performs a login."""
-        if self.can_login:
-            try:
-                self.passwd.verify(passwd)
-            except VerifyMismatchError:
-                self.failed_logins += 1
-                self.save()
-                raise InvalidCredentials()
-
-            if self.passwd.needs_rehash:
-                self.passwd = passwd
-
-            self.failed_logins = 0
-            self.last_login = datetime.now()
-            self.save()
-            return True
-
-        raise UserLocked()
+        return not self.locked and not self.expired
 
     def patch_json(self, json, **kwargs):
         """Patches the user with the respective JSON data."""
-        try:
-            tenement = json.pop('tenement')
-        except KeyError:
-            tenement = self.tenement
-        else:
+        tenement = json.pop('tenement', None)
+
+        if tenement is not None:
             self.tenement = get_tenement(tenement, self.customer)
 
         super().patch_json(json, **kwargs)
 
-    def to_json(self, cascade=False, **kwargs):
+    def to_json(self, tenement=False, **kwargs):
         """Returns JSON-ish dict."""
         dictionary = super().to_json(**kwargs)
 
-        if cascade and self.tenement is not None:
-            dictionary['tenement'] = self.tenement.to_json(
-                cascade=cascade, **kwargs)
+        if tenement and self.tenement is not None:
+            dictionary['tenement'] = self.tenement.to_json(**kwargs)
 
         return dictionary
