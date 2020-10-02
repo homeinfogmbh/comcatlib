eff545379d43c469bcf89ccfdc515bb5ed4cbe13
Richard Neumann <r.neumann@homeinfo.de>
2020-10-02 13:48:20 +0200

    Enabled refresh token generation as per https://docs.authlib.org/en/stable/flask/2/grants.html?highlight=RefreshTokenGrant#refresh-token-grant

--- comcatlib/oauth/refresh_token_grant.py
+++ comcatlib/oauth/refresh_token_grant.py
@@ -13,6 +13,7 @@ class RefreshTokenGrant(grants.RefreshTokenGrant):
     """Handles refresh token grants."""
 
     TOKEN_ENDPOINT_AUTH_METHODS = ['client_secret_post']
+    INCLUDE_NEW_REFRESH_TOKEN = True
 
     def authenticate_refresh_token(self, refresh_token):
         """Authenticates the refresh token."""
