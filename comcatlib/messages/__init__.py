e44bb379fb1f4dbf7cc90baaadee3a4e3b072f2c
Richard Neumann <mail@richard-neumann.de>
2021-01-29 12:57:00 +0100

    Removed damage report related messages.

--- comcatlib/messages/__init__.py
+++ comcatlib/messages/__init__.py
@@ -5,19 +5,12 @@ from comcatlib.messages.address import ADDRESS_DELETED
 from comcatlib.messages.address import INVALID_ADDRESS
 from comcatlib.messages.address import MISSING_ADDRESS
 from comcatlib.messages.address import NO_SUCH_ADDRESS
-from comcatlib.messages.attachments import ATTACHMENT_ADDED
-from comcatlib.messages.attachments import ATTACHMENT_DELETED
-from comcatlib.messages.attachments import NO_SUCH_ATTACHMENT
 from comcatlib.messages.base_chart_menu import BASE_CHART_MENU_ADDED
 from comcatlib.messages.base_chart_menu import BASE_CHART_MENU_DELETED
 from comcatlib.messages.base_chart_menu import NO_SUCH_BASE_CHART_MENU
 from comcatlib.messages.clientreg import INVALID_UUID
 from comcatlib.messages.clientreg import INVALID_NONCE
 from comcatlib.messages.clientreg import MISSING_NONCE
-from comcatlib.messages.damage_report import DAMAGE_REPORT_ALREADY_PROCESSED
-from comcatlib.messages.damage_report import DAMAGE_REPORT_DELETED
-from comcatlib.messages.damage_report import DAMAGE_REPORT_SUBMITTED
-from comcatlib.messages.damage_report import NO_SUCH_DAMAGE_REPORT
 from comcatlib.messages.files import NO_SUCH_FILE
 from comcatlib.messages.files import QUOTA_EXCEEDED
 from comcatlib.messages.news import NEWS_NOT_ENABLED
@@ -43,23 +36,16 @@ from comcatlib.messages.user import USER_PATCHED
 
 __all__ = [
     'ADDRESS_ADDED',
-    'ATTACHMENT_DELETED',
     'ADDRESS_DELETED',
     'INVALID_ADDRESS',
     'MISSING_ADDRESS',
     'NO_SUCH_ADDRESS',
-    'ATTACHMENT_ADDED',
-    'NO_SUCH_ATTACHMENT',
     'BASE_CHART_MENU_ADDED',
     'BASE_CHART_MENU_DELETED',
     'NO_SUCH_BASE_CHART_MENU',
     'INVALID_UUID',
     'INVALID_NONCE',
     'MISSING_NONCE',
-    'DAMAGE_REPORT_ALREADY_PROCESSED',
-    'DAMAGE_REPORT_DELETED',
-    'DAMAGE_REPORT_SUBMITTED',
-    'NO_SUCH_DAMAGE_REPORT',
     'NO_SUCH_FILE',
     'QUOTA_EXCEEDED',
     'NEWS_NOT_ENABLED',
