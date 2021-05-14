7bae1b58390bcf3359c98422bbcea84411f2bffd
Richard Neumann <mail@richard-neumann.de>
2021-05-14 14:08:33 +0200

    Refactor for new Presentation style

--- comcatlib/presentation.py
+++ comcatlib/presentation.py
@@ -1,9 +1,10 @@
 """Presentations for ComCat accounts."""
 
-from typing import Iterable
+from typing import Iterator
 
-from cmslib import NoConfigurationFound
+from cmslib import BaseChart
 from cmslib import Configuration
+from cmslib import Group
 from cmslib import Menu
 from cmslib import Presentation
 from cmslib import get_trashed
@@ -32,33 +33,28 @@ class Presentation(Presentation):   # pylint: disable=E0102
     @property
     def customer(self):
         """Returns the customer."""
-        return user.tenement.customer
+        return self.user.tenement.customer
 
-    @property
-    def base_charts(self) -> Iterable[UserBaseChart]:
+    def get_base_charts(self) -> Iterator[BaseChart]:
         """Yields the user's base charts."""
-        return UserBaseChart.select(cascade=True).where(
-            (UserBaseChart.user == self.user) & get_trashed()
-        ).order_by(UserBaseChart.index)
+        for user_base_chart in UserBaseChart.select(cascade=True).where(
+                (UserBaseChart.user == self.user) & get_trashed()).order_by(
+                UserBaseChart.index):
+            yield (user_base_chart.index, user_base_chart.base_chart)
 
-    @property
-    def configuration(self) -> Configuration:
+    def get_configurations(self) -> Iterator[Configuration]:
         """Returns the user's configuration."""
-        try:
-            return Configuration.select(cascade=True).where(
-                UserConfiguration.user == self.user).get()
-        except Configuration.DoesNotExist:
-            raise NoConfigurationFound() from None
+        for user_configuration in UserConfiguration.select(cascade=True).where(
+                UserConfiguration.user == self.user):
+            yield user_configuration.configuration
 
-    @property
-    def groups(self) -> GroupMemberUser:
+    def get_memberships(self) -> Iterator[Group]:
         """Yields groups this user is a member of."""
         for gma in GroupMemberUser.select(cascade=True).where(
                 GroupMemberUser.user == self.user):
             yield gma.group
 
-    @property
-    def menus(self) -> Menu:
+    def get_menus(self) -> Iterator[Menu]:
         """Yields menus of this user."""
         return Menu.select(cascade=True).join_from(Menu, UserMenu).where(
             UserMenu.user == self.user)
