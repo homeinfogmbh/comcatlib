d1d092c542eb18ec66c759cd93e01fbad624f858
Richard Neumann <mail@richard-neumann.de>
2021-07-07 10:32:34 +0200

    Add email address

--- comcatlib/orm/registration.py
+++ comcatlib/orm/registration.py
@@ -20,21 +20,25 @@ class UserRegistration(ComCatModel):
 
     first_name = CharField()
     last_name = CharField()
+    email = CharField()
     tenant_id = CharField()
     customer = ForeignKeyField(
         Customer, column_name='customer', on_delete='CASCADE')
     registered = DateTimeField(default=datetime.now)
 
     @classmethod
-    def add(cls, first_name: str, last_name: str, tenant_id: str,
+    def add(cls, first_name: str, last_name: str, email: str,tenant_id: str,
             customer: Union[Customer, int]) -> UserRegistration:
         """Adds a new user registration."""
         try:
             record = cls.select().where(
-                (cls.tenant_id == tenant_id) & (cls.customer == customer)
+                (
+                    (cls.tenant_id == tenant_id)
+                    | (cls.email == email)
+                ) & (cls.customer == customer)
             ).get()
         except cls.DoesNotExist:
-            return cls(first_name=first_name, last_name=last_name,
+            return cls(first_name=first_name, last_name=last_name, email=email,
                        tenant_id=tenant_id, customer=customer)
 
         raise AlreadyRegistered(record)
