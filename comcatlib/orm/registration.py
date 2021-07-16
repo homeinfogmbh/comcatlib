f36a415ee200a6e0277878fe7d434fad41c6c38e
Richard Neumann <r.neumann@homeinfo.de>
2021-07-16 16:40:41 +0200

    Refactor duplicate selection into seperate methods

--- comcatlib/orm/registration.py
+++ comcatlib/orm/registration.py
@@ -4,7 +4,7 @@ from __future__ import annotations
 from datetime import datetime
 from typing import Union
 
-from peewee import CharField, DateTimeField, ForeignKeyField
+from peewee import CharField, DateTimeField, ForeignKeyField, ModelSelect
 
 from mdb import Customer
 
@@ -29,16 +29,24 @@ class UserRegistration(ComCatModel):    # pylint: disable=R0903
     registered = DateTimeField(default=datetime.now)
 
     @classmethod
+    def same_ids_sel(cls, tenant_id: str, email: str) -> ModelSelect:
+        """Returns a select condition to match records with the same IDs."""
+        return (cls.tenant_id == tenant_id) | (cls.email == email)
+
+    @classmethod
+    def dupes_select(cls, tenant_id: str, email: str,
+                     customer: Union[Customer, int]) -> ModelSelect:
+        """Returns a select condition to match duplicales."""
+        return cls.same_ids_sel(tenant_id, email) & (cls.customer == customer)
+
+    @classmethod
     def add(cls, name: str, email: str, tenant_id: str,
             customer: Union[Customer, int]) -> UserRegistration:
         """Adds a new user registration."""
+        condition = cls.dupes_select(tenant_id, email, customer)
+
         try:
-            record = cls.select().where(
-                (
-                    (cls.tenant_id == tenant_id)
-                    | (cls.email == email)
-                ) & (cls.customer == customer)
-            ).get()
+            record = cls.select().where(condition).get()
         except cls.DoesNotExist:
             return cls(name=name, email=email, tenant_id=tenant_id,
                        customer=customer)
