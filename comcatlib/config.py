b9eba0aeb5b5f2fdd4163d1965069de983eb3c86
Richard Neumann <mail@richard-neumann.de>
2021-08-30 12:10:04 +0200

    Add user registration template

--- comcatlib/config.py
+++ comcatlib/config.py
@@ -1,6 +1,7 @@
 """ComCat configuration."""
 
 from json import load
+from pathlib import Path
 
 from configlib import loadcfg
 
@@ -9,13 +10,15 @@ __all__ = [
     'CONFIG',
     'ALLOWED_SESSION_DURATIONS',
     'DEFAULT_SESSION_DURATION',
-    'OAUTH2'
+    'OAUTH2',
+    'USER_REG_TEMP'
 ]
 
 
 CONFIG = loadcfg('comcat.conf')
 ALLOWED_SESSION_DURATIONS = range(5, 31)
 DEFAULT_SESSION_DURATION = 15
+USER_REG_TEMP = Path('/usr/local/etc/comcat.d/userreg.temp')
 
 
 with open('/usr/local/etc/comcat.d/oauth2.json', 'r') as oauth2:
