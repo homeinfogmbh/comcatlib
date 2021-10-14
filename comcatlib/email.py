1b7fc16dc416d44d77b6110722e0394185c6fe21
Richard Neumann <mail@richard-neumann.de>
2021-10-14 13:49:11 +0200

    Reduce email.py

--- comcatlib/email.py
+++ comcatlib/email.py
@@ -1,32 +1,12 @@
 """Emailing."""
 
-from pathlib import Path
-
-from emaillib import EMail, Mailer
+from emaillib import Mailer
 
 from comcatlib.config import CONFIG
 
 
-__all__ = [
-    'MAILER',
-    'SENDER',
-    'USER_REG_SUBJECT',
-    'USER_REG_TEMP',
-    'make_user_registration_email'
-]
+__all__ = ['MAILER', 'SENDER']
 
 
 MAILER = Mailer.from_config(CONFIG)
-SENDER = 'noreply@homeinfo.de'
-USER_REG_SUBJECT = 'Mieter-App'
-USER_REG_TEMP = Path('/usr/local/etc/comcat.d/userreg.temp')
-
-
-def make_user_registration_email(email: str, login: int, passwd: str) -> EMail:
-    """Creates a user registration email."""
-
-    with USER_REG_TEMP.open(encoding='utf-8') as file:
-        template = file.read()
-
-    text = template.format(name=login, passwd=passwd)
-    return EMail(USER_REG_SUBJECT, SENDER, email, plain=text)
+SENDER = 'mieterapp@homeinfo.de'
