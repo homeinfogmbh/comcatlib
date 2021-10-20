ac8f8856aa263508629c3f09d237f1ebaa94e0bd
Richard Neumann <mail@richard-neumann.de>
2021-10-20 11:56:26 +0200

    Use pathlib and add encoding

--- comcatlib/config.py
+++ comcatlib/config.py
@@ -1,6 +1,7 @@
 """ComCat configuration."""
 
 from json import load
+from pathlib import Path
 
 from configlib import loadcfg
 
@@ -16,7 +17,8 @@ __all__ = [
 CONFIG = loadcfg('comcat.conf')
 ALLOWED_SESSION_DURATIONS = range(5, 31)
 DEFAULT_SESSION_DURATION = 15
+OAUTH2_JSON = Path('/usr/local/etc/comcat.d/oauth2.json')
 
 
-with open('/usr/local/etc/comcat.d/oauth2.json', 'r') as oauth2:
+with OAUTH2_JSON.open('r', encoding='utf-8') as oauth2:
     OAUTH2 = load(oauth2)
