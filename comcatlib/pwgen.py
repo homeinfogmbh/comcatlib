398295d077fd0ebf59aecc43b17d3eee60b6cb13
Richard Neumann <mail@richard-neumann.de>
2021-10-21 23:29:33 +0200

    Use secrets.choice()

--- comcatlib/pwgen.py
+++ comcatlib/pwgen.py
@@ -1,13 +1,13 @@
 """Common functions."""
 
-from random import choices
+from secrets import choice
 from string import ascii_letters, digits
 
 
 __all__ = ['genpw']
 
 
-def genpw(*, pool: str = ascii_letters+digits, length: int = 32) -> str:
+def genpw(*, pool: str = ascii_letters + digits, length: int = 32) -> str:
     """Generates a random password."""
 
-    return ''.join(choices(pool, k=length))
+    return ''.join(choice(pool) for _ in range(length))
