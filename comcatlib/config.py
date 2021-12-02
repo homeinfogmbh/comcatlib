da862f6d3bfc83c5e8f3e0105cbd1ec80dfea4b5
Richard Neumann <r.neumann@homeinfo.de>
2021-12-02 12:34:41 +0100

    Cache config files

--- comcatlib/config.py
+++ comcatlib/config.py
@@ -1,6 +1,6 @@
 """ComCat configuration."""
 
-from functools import partial
+from functools import cache, partial
 from pathlib import Path
 
 from configlib import load_config
@@ -21,5 +21,6 @@ DEFAULT_SESSION_DURATION = 15
 OAUTH2_JSON = Path('/usr/local/etc/comcat.d/oauth2.json')
 
 
+load_config = cache(load_config)
 get_config = partial(load_config, CONFIG_FILE)
 get_oauth2 = partial(load_config, OAUTH2_JSON)
