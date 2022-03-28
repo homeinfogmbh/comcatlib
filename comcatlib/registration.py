75c26f201f7406daedbda03da5fd74034481c365
Richard Neumann <r.neumann@homeinfo.de>
2022-03-28 13:31:27 +0200

    Rewrite notify_customer()

--- comcatlib/registration.py
+++ comcatlib/registration.py
@@ -1,13 +1,11 @@
 """Registration related functions."""
 
-from collections import defaultdict
 from pathlib import Path
-from typing import Iterable, Iterator, Optional
-from xml.etree.ElementTree import Element, SubElement
+from typing import Iterator, Optional
+from xml.etree.ElementTree import Element, SubElement, tostring
 
 from emaillib import EMail
 from mdb import Customer
-from notificationlib import get_email_func
 
 from comcatlib.email import SENDER, get_mailer
 from comcatlib.orm.registration import UserRegistration
@@ -21,68 +19,55 @@ NOTIFICATION_SUBJECT = 'Neue Benutzerregistrierungen für Ihre App'
 SUBJECT = 'Mieter-App'
 USER_CONFIRM_TEMP = Path('/usr/local/etc/comcat.d/user-confirmation.temp')
 USER_REG_TEMP = Path('/usr/local/etc/comcat.d/user-registration.temp')
-RegistrationMap = dict[Customer, list[UserRegistration]]
 
 
-def to_html(user_registrations: Iterable[UserRegistration]) -> Element:
+def notify_customer(user_registration: UserRegistration) -> None:
+    """Notify customer via email about a user registration event."""
+
+    get_mailer().send(get_customer_emails(user_registration))
+
+
+def notify_user(email: str, passwd: Optional[str] = None) -> None:
+    """Sends a notification email to the registered email address."""
+
+    get_mailer().send([
+        EMail(SUBJECT, SENDER, email, plain=get_body(email, passwd))
+    ])
+
+
+def get_customer_emails(
+        user_registration: UserRegistration
+) -> Iterator[EMail]:
+    """Yields emails to send."""
+
+    html = to_html(user_registration)
+
+    for notification_email in RegistrationNotificationEmails.select().where(
+            Customer == user_registration.customer
+    ):
+        yield EMail(
+            NOTIFICATION_SUBJECT, SENDER, notification_email.email, html=html
+        )
+
+
+def to_html(user_registration: UserRegistration) -> str:
     """Converts user registrations into an HTML element."""
 
     html = Element('html')
     body = SubElement(html, 'body')
     h1 = SubElement(body, 'h1')
-    h1.text = 'Neue Registrierungen'
+    h1.text = 'Neue Registrierung'
     p = SubElement(body, 'p')
-    p.text = 'Folgende Benutzer haben sich für die App registriert:'
+    p.text = 'Folgender Benutzer hat sich für die App registriert:'
     SubElement(body, 'br')
     table = SubElement(body, 'table', attrs={'border': '1'})
-
-    for user_registration in user_registrations:
-        table.append(user_registration.to_html())
-
+    table.append(user_registration.to_html())
     p = SubElement(body, 'p')
     p.text = 'Mit freundlichen Grüßen'
     SubElement(body, 'br')
     p = SubElement(body, 'p')
     p.text = 'Ihre HOMEINFO GmbH'
-    return html
-
-
-def to_emails(
-        customer: Customer,
-        user_registrations: Iterable[UserRegistration]
-) -> Iterator[EMail]:
-    """Converts a user registration records into an email and deletes them."""
-
-    if not user_registrations:
-        return
-
-    for notification_email in RegistrationNotificationEmails.select().where(
-            Customer == customer
-    ):
-        yield EMail(
-            NOTIFICATION_SUBJECT, SENDER, notification_email.email,
-            html=to_html(user_registrations)
-        )
-
-
-def get_user_registrations_by_customer() -> RegistrationMap:
-    """Returns a dict of customer -> UserRegistration."""
-
-    result = defaultdict(list)
-
-    for user_registration in UserRegistration.of_today():
-        result[user_registration.customer].append(user_registration)
-
-    return result
-
-
-def get_emails() -> Iterator[EMail]:
-    """Yields emails to send."""
-
-    user_registrations_by_customer = get_user_registrations_by_customer()
-
-    for customer, user_registrations in user_registrations_by_customer.items():
-        yield from to_emails(customer, user_registrations)
+    return tostring(html, encoding='unicode', method='html')
 
 
 def get_body(email: str, passwd: Optional[str] = None) -> str:
@@ -94,14 +79,3 @@ def get_body(email: str, passwd: Optional[str] = None) -> str:
 
     with USER_CONFIRM_TEMP.open(encoding='utf-8') as file:
         return file.read().format(name=email, passwd=passwd)
-
-
-def notify_user(email: str, passwd: Optional[str] = None) -> None:
-    """Sends a notification email to the registered email address."""
-
-    get_mailer().send([
-        EMail(SUBJECT, SENDER, email, plain=get_body(email, passwd))
-    ])
-
-
-notify_customer = get_email_func(get_emails)
