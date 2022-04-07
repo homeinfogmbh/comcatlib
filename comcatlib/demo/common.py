1ae9141d9bba2ae777cb215ccf56cbd9fbf29c90
Richard Neumann <r.neumann@homeinfo.de>
2022-04-07 15:10:49 +0200

    Add demo data paths

--- comcatlib/demo/common.py
+++ comcatlib/demo/common.py
@@ -1,13 +1,21 @@
 """Common constants and functions."""
 
 from datetime import datetime
+from pathlib import Path
 from random import randint
 
 
-__all__ = ['DEMO_CUSTOMER_ID', 'randdate']
+__all__ = [
+    'DEMO_CUSTOMER_ID',
+    'DEMO_DATASET_FILE',
+    'DEMO_DATASET_ATTACHMENTS',
+    'randdate'
+]
 
 
 DEMO_CUSTOMER_ID = 5000
+DEMO_DATASET_FILE = Path('/usr/local/etc/comcat.d/demo.json')
+DEMO_DATASET_ATTACHMENTS = Path('/usr/local/etc/comcat.d/demo.d')
 
 
 def randdate(start: datetime, end: datetime) -> datetime:
