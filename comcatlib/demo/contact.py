07e01cb15186ec29c94f3ad76d5dd4deea20b9d7
Richard Neumann <r.neumann@homeinfo.de>
2022-04-08 15:37:36 +0200

    Simplify chart creation

--- comcatlib/demo/contact.py
+++ comcatlib/demo/contact.py
@@ -1,7 +1,5 @@
 """Contact chart creation."""
 
-from cmslib import Chart
-
 from comcatlib.demo.image_text import create_image_text_chart
 from comcatlib.orm.menu import Menu, MenuBaseChart
 
@@ -13,11 +11,5 @@ def create_contact(contact: dict) -> None:
     """Creates a contact chart."""
 
     chart = create_image_text_chart(contact['title'], contact['text'])
-    make_contact(chart)
-
-
-def make_contact(chart: Chart) -> None:
-    """Make the chart a contact chart."""
-
     menu_base_chart = MenuBaseChart(base_chart=chart.base, menu=Menu.CONTACT)
     menu_base_chart.save()
