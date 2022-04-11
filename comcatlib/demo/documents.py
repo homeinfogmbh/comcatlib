fd7ec48b43ae1ef787a743f230cf623b6eaea506
Richard Neumann <r.neumann@homeinfo.de>
2022-04-11 11:08:57 +0200

    Rename parameter

--- comcatlib/demo/documents.py
+++ comcatlib/demo/documents.py
@@ -7,9 +7,9 @@ from comcatlib.orm.menu import Menu, MenuBaseChart
 __all__ = ['create_documents_chart']
 
 
-def create_documents_chart(service: dict) -> None:
+def create_documents_chart(documents: dict) -> None:
     """Creates a documents chart."""
 
-    chart = create_image_text_chart(service['title'], service['text'])
+    chart = create_image_text_chart(documents['title'], documents['text'])
     menu_base_chart = MenuBaseChart(base_chart=chart.base, menu=Menu.DOCUMENTS)
     menu_base_chart.save()
