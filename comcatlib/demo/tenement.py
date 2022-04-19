de32b9e974e10e71dff14f92921e2fffcb4aa36c
Richard Neumann <r.neumann@homeinfo.de>
2022-04-19 14:37:52 +0200

    Suppress Integrity errors

--- comcatlib/demo/tenement.py
+++ comcatlib/demo/tenement.py
@@ -2,7 +2,11 @@
 
 from typing import Union
 
+from peewee import IntegrityError
+
 from mdb import Address, Customer, Tenement
+
+from comcatlib.demo.common import LOGGER
 
 
 __all__ = ['create_tenement', 'delete_tenements']
@@ -24,7 +28,15 @@ def create_tenement(
     return tenement
 
 
-def delete_tenements(customer: Union[Customer, int]) -> int:
+def delete_tenements(customer: Union[Customer, int]) -> None:
     """Deletes all tenements of the given customer."""
 
-    return Tenement.delete().where(Tenement.customer == customer).execute()
+    for tenement in Tenement.select().join(Address).where(
+            Tenement.customer == customer
+    ):
+        try:
+            tenement.delete_instance()
+        except IntegrityError:
+            LOGGER.warning('Cannot delete tenement: %s', tenement.address)
+        else:
+            LOGGER.info('Deleted tenement: %s', tenement.address)
