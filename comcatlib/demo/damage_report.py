cb714e74947bbe004236c1bf67684d9423b13d56
Richard Neumann <r.neumann@homeinfo.de>
2022-04-19 12:52:58 +0200

    Move exported functions up

--- comcatlib/demo/damage_report.py
+++ comcatlib/demo/damage_report.py
@@ -14,34 +14,6 @@ from comcatlib.orm.damage_report import UserDamageReport
 __all__ = ['create_damage_reports', 'delete_damage_reports']
 
 
-def create_damage_report(
-        user: User,
-        message: str,
-        damage_type: str,
-        timestamp: datetime
-) -> None:
-    """Creates a dummy damage report of the given user."""
-
-    LOGGER.info('Adding damage report: %s (%s)', message, damage_type)
-    damage_report = DamageReport(
-        customer=user.tenement.customer,
-        address=user.tenement.address,
-        message=message,
-        name=user.name,
-        damage_type=damage_type,
-        timestamp=timestamp
-    )
-    damage_report.save()
-    LOGGER.info(
-        'Mapping damage report %i to user %s', damage_report.id, user.email
-    )
-    user_damage_report = UserDamageReport(
-        user=user,
-        damage_report=damage_report
-    )
-    user_damage_report.save()
-
-
 def create_damage_reports(
         users: Sequence[User],
         damage_reports: Sequence[dict]
@@ -69,3 +41,31 @@ def delete_damage_reports(customer: Customer) -> None:
             damage_report.damage_type
         )
         damage_report.delete_instance()
+
+
+def create_damage_report(
+        user: User,
+        message: str,
+        damage_type: str,
+        timestamp: datetime
+) -> None:
+    """Creates a dummy damage report of the given user."""
+
+    LOGGER.info('Adding damage report: %s (%s)', message, damage_type)
+    damage_report = DamageReport(
+        customer=user.tenement.customer,
+        address=user.tenement.address,
+        message=message,
+        name=user.name,
+        damage_type=damage_type,
+        timestamp=timestamp
+    )
+    damage_report.save()
+    LOGGER.info(
+        'Mapping damage report %i to user %s', damage_report.id, user.email
+    )
+    user_damage_report = UserDamageReport(
+        user=user,
+        damage_report=damage_report
+    )
+    user_damage_report.save()
