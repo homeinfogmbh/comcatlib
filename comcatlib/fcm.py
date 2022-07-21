1aea464b2933452c66a5400d51194fb4dd55d0b9
Richard Neumann <r.neumann@homeinfo.de>
2022-07-21 13:06:09 +0200

    Use library to create tokens

---
+++ comcatlib/fcm.py
@@ -0,0 +1,100 @@
+"""Firebase Cloud Messaging."""
+
+from logging import getLogger
+from typing import Union
+
+from firebase_admin import App, initialize_app
+from firebase_admin.messaging import AndroidConfig
+from firebase_admin.messaging import AndroidNotification
+from firebase_admin.messaging import APNSConfig
+from firebase_admin.messaging import APNSPayload
+from firebase_admin.messaging import Aps
+from firebase_admin.messaging import Message
+from firebase_admin.messaging import Notification
+from firebase_admin.messaging import WebpushConfig
+from firebase_admin.messaging import WebpushNotification
+from firebase_admin.messaging import WebpushNotificationAction
+from firebase_admin.messaging import send
+
+from comcatlib.orm import FCMToken, User
+
+
+__all__ = ['add_token', 'delete_tokens', 'init']
+
+
+LOGGER = getLogger(__file__)
+
+
+def add_token(user: Union[User, int]) -> str:
+    """Generates a new FCM token."""
+
+    fcm_token = FCMToken.for_user(user=user)
+    fcm_token.save()
+    return fcm_token.token
+
+
+def delete_tokens(user: Union[User, int], *tokens: str) -> None:
+    """Deletes and existing FCM token."""
+
+    condition = FCMToken.user == user
+
+    if tokens:
+        condition &= FCMToken.token << tokens
+
+    for fcm_token in FCMToken.select().where(condition):
+        fcm_token.delete_instance()
+
+
+def init() -> App:
+    """Initialize the firebase app."""
+
+    return initialize_app()
+
+
+def send_message(
+        token: str,
+        *,
+        urlcode: str,
+        title: str,
+        body: str
+) -> str:
+    """Sends a message."""
+
+    return send(
+        Message(
+            notification=Notification(title=title, body=body),
+            android=AndroidConfig(
+                notification=AndroidNotification(click_action=urlcode)
+            ),
+            apns=APNSConfig(
+                payload=APNSPayload(
+                    aps=Aps(custom_data={'urlcode': urlcode})
+                )
+            ),
+            webpush=WebpushConfig(
+                notification=WebpushNotification(
+                    actions=[WebpushNotificationAction(urlcode, 'öffnen')]
+                )
+            ),
+            token=token
+        )
+    )
+
+
+def message_user(
+        user: Union[User, int],
+        *,
+        urlcode: str,
+        title: str,
+        body: str
+) -> dict[str, str]:
+    """Message a user."""
+
+    results = {}
+
+    for token in FCMToken.select().where(FCMToken.user == user):
+        results[token] = send_message(
+            token := token.token, urlcode=urlcode, title=title, body=body
+        )
+
+    return results
--- comcatlib/orm/fcm_token.py
+++ comcatlib/orm/fcm_token.py
@@ -1,10 +1,11 @@
 """Firebase Cloud Messaging tokens."""
 
+from __future__ import annotations
+from firebase_admin.auth import create_custom_token
 from peewee import CharField, ForeignKeyField
 
 from comcatlib.orm.common import ComCatModel
 from comcatlib.orm.user import User
-from comcatlib.pwgen import genpw
 
 
 __all__ = ['FCMToken']
@@ -17,4 +18,9 @@ class FCMToken(ComCatModel):
         table_name = 'fcm_token'
 
     user = ForeignKeyField(User, column_name='user', on_delete='CASCADE')
-    token = CharField(162, default=lambda: genpw(length=162), unique=True)
+    token = CharField(162, unique=True)
+
+    @classmethod
+    def for_user(cls, user: User) -> FCMToken:
+        """Creates a new FCM token for the given user."""
+        return cls(user=user, token=create_custom_token(user.email).decode())
